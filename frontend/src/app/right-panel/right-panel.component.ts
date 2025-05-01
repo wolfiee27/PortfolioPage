6c36cf60811b88afcbe4b7b9ed3227ffcc4a67a7
Gokul <36274337+wolfiee27@users.noreply.github.com>
2025-05-01 12:28:10 +0530

    created right panel

---
+++ frontend/src/app/right-panel/right-panel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-right-panel',
+  imports: [],
+  templateUrl: './right-panel.component.html',
+  styleUrl: './right-panel.component.scss'
+})
+export class RightPanelComponent {
+
+}
